ed8e95bac6a79a2225343744788799992920c919
Orbel <ogolanians@gmail.com>
2021-09-01 22:26:43 -0700

    Skeleton setup for array question CyclicRotation

---
+++ Arrays/1-CyclicRotation.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+/*
+ Rotate array to right N times.
+ https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+ 
+ For example, given
+
+     A = [3, 8, 9, 7, 6]
+     K = 3
+ the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+     [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+     [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+
+ */
+
+func solution(A: [Int], K: Int) -> [Int] {
+    var result = A
+    return result
+}
+
+solution(A: [3, 8, 9, 7, 6], K: 3)
+
+
