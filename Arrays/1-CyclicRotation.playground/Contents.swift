432446d63954b06a9c77fedfbe2108094a51b492
Orbel <ogolanians@gmail.com>
2021-09-01 23:06:26 -0700

    Completed CyclicRotation using array manipulation

--- Arrays/1-CyclicRotation.playground/Contents.swift
+++ Arrays/1-CyclicRotation.playground/Contents.swift
@@ -16,10 +16,40 @@ import UIKit
 
  */
 
+// Time: O(n^2)
 func solution(A: [Int], K: Int) -> [Int] {
+    guard !A.isEmpty else { return [] }
+    guard K > 0 else { return A }
+    
     var result = A
+    for _ in 0...K-1 { // O(n)
+        result = rotateArrByOne(result)
+    }
     return result
 }
+
+func rotateArrByOne(_ A: [Int]) -> [Int] {
+    var newArr = Array<Int>(repeating: 0, count: A.count)
+    // First iteration we will have 0,3,0,0,0
+    // We will use original array A to fill up remaining elements in newArr
+    for i in 0..<newArr.count - 1 { // O(n)
+        newArr[i+1] = A[i]
+    }
+    // Due to array size, the last index gets overriden
+    // We can simply use original array to assign it to 1st element in newArr
+    newArr[0] = A.last!
+    
+    return newArr
+}
+solution(A: [], K: 0)
+solution(A: [], K: 1)
+solution(A: [-4], K: 0)
+solution(A: [-4], K: 1)
+solution(A: [1, 2, 3], K: 4)
+
+solution(A: [1, 2, 3, 4, 5], K: 1)
+solution(A: [1, 2, 3, 4, 5], K: 2)
+solution(A: [1, 2, 3, 4, 5], K: 3)
 
 solution(A: [3, 8, 9, 7, 6], K: 3)
 
