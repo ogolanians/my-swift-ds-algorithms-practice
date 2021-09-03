95740599994c6cc49b04978fcba05968c4ea4fff
Orbel <ogolanians@gmail.com>
2021-09-03 13:06:30 -0700

    Added insert function in BST class

--- BinaryTrees/BST.playground/Contents.swift
+++ BinaryTrees/BST.playground/Contents.swift
@@ -10,8 +10,35 @@ class Node {
     }
 }
 
+let bst = BST()
+bst.insert(key: 5)
+bst.insert(key: 3)
+bst.insert(key: 2)
+bst.insert(key: 4)
+bst.insert(key: 7)
+bst.insert(key: 6)
+bst.insert(key: 8)
+
 class BST {
     var root: Node?
+    
+    func insert(key: Int) {
+        root = insertItem(root, key)
+    }
+    
+    private func insertItem(_ node: Node?, _ key: Int) -> Node {
+        guard let node = node else {
+            let node = Node(key)
+            return node
+        }
+        if key < node.key {
+            node.left = insertItem(node.left, key)
+        }
+        if key > node.key {
+            node.right = insertItem(node.right, key)
+        }
+        return node
+    }
     
     func find(key: Int) -> Int? {
         guard let root = root else { return nil }
