95021f1868c3b91660edcf7dddfc1da96b30d628
Orbel <ogolanians@gmail.com>
2021-09-04 17:48:23 -0700

    Added ContactMatch question in Arrays

---
+++ Arrays/3-ContactMatch.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/*
+ When someone searches their contacts based on a phone number, it's nice when a list
+ of contact pops up.
+ 
+ Write an algorithm that searches your contacts for phone number strings, and returns:
+  - NO CONTACT if contact can't be found
+  - A contact if a contact is found
+  - The first alphabetized contact if there are multiple
+ 
+ A = ["pim", "pom"]             // Contacts (return one of these)
+ B = ["", "999999999777888999"] // Phone numbers for each contact A[i] = B[i]
+ P = "88999"                    // Search phrase
+
+ Answer should be correct. Don't worry about performance.
+ */
+
+
